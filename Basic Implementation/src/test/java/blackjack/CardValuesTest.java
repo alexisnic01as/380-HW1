31dfdfca78a3bd1ea29c55cd27268bef3d47c6a1
Marc Shen <marcjshen@gmail.com>
2022-01-17 18:05:00 -0600

    created test classes

---
+++ Basic Implementation/src/test/java/blackjack/CardValuesTest.java
@@ -0,0 +1,9 @@
+/**
+ * The class implements unit tests for the CardValues class from the blackjack package.
+ *
+ * @author Alexis Nicolas (atn4), Marc Shen (mjs31), Jonathan Sheng (js165)
+ */
+
+public class CardValuesTest {
+
+}
---
+++ Basic Implementation/src/test/java/card/CardTest.java
@@ -0,0 +1,9 @@
+/**
+ * The class implements unit tests for the Card class from the card package.
+ *
+ * @author Alexis Nicolas (atn4), Marc Shen (mjs31), Jonathan Sheng (js165)
+ */
+
+public class CardTest {
+
+}
